373574d5eb1c4c8b22debdc13f1f631b2bf431a8
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2024-06-04 22:45:10 +0500

    update colorsEXAMPLE.lua

--- colorsEXAMPLE.lua
+++ colorsEXAMPLE.lua
@@ -43,6 +43,11 @@ local ScreenGui = Create("ScreenGui", {
             Size = UDim2.new(1,0,1,-40),
             Position = UDim2.fromScale(0,1),
             AnchorPoint = Vector2.new(0,1),
+        }, {
+            Create("UIListLayout", {
+                FillDirection = "Vertical",
+                Padding = UDim.new(0,8)
+            })
         })
     })
 })
